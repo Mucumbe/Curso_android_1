d8fcb11c5618630afb24c8faeb96203b9e367cb2
Mucumbe <mucumbeblandino@gmail.com>
2022-06-07 21:24:27 +0200

    Exercicio Numero aleatorio

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,10 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public  void numeroAleatorio(View view){
+        Random nA= new Random();
 
-    public void alterarTexto(View view){
-
-        TextView texto= findViewById(R.id.aterarTexto);
-        texto.setText("Blandino Mucumbe");
+        int numero=nA.nextInt(11);
+        System.out.println("kkkkkk"+numero);
+        String informacao="O núnero selecionado é: ";
+        TextView aleatorioTexto=findViewById(R.id.nAleatorio);
+        aleatorioTexto.setText(informacao+numero);
     }
+   // public void alterarTexto(View view){
+       // TextView texto= findViewById(R.id.aterarTexto);
+        //texto.setText("Blandino Mucumbe");
+    //}
 }
